cfd8ac6a2fe607fe477294f5f649e59e038169cf
Ferdi Tolenaar <f.tolenaar@gmail.com>
2025-12-25 11:54:21 +0800

    link

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import pb from '../lib/pocketbase';
 
 export default function RegisterPage() {
