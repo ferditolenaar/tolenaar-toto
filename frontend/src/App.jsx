a936f83c651e62005eab583ac2ac0f3e20986523
Ferdi Tolenaar <f.tolenaar@gmail.com>
2025-12-25 11:03:40 +0800

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import StartPage from './pages/StartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import StartPage from './pages/StartPage';
+import './App.css'; // Make sure the CSS is imported here!
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        {/* Everything inside this Route gets the Navbar */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
