a24ac05720c6a5782caf2526185215ed2439e57a
Ferdi Tolenaar <f.tolenaar@gmail.com>
2026-06-15 13:38:23 +0800

    NL language

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Actively force the HTML document language to Dutch
+document.documentElement.lang = 'nl';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
